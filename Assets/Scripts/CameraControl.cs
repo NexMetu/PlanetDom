014b8cf4e01a6c5899ecb09c46c0fffe610061ff
Matthew Lang <Mattlang9000@gmail.com>
2017-03-01 14:53:02 +1300

    working on the turn system
    turn system adding to planets+
    turn button+

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public float speed = 2f;
+
+	public float sensitivity = 2f;
+
+	bool atTopWall = false;
+	bool atBtmWall = false;
+	bool atRightWall = false;
+	bool atLeftWall = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void onTriggerEnter2D(Collider2D other) {
+		if(other.tag == "LeftWall"){
+			atLeftWall = true;
+		} else if (other.tag == "RightWall"){
+			atRightWall = true;
+		} else if (other.tag == "TopWall"){
+			atTopWall = true;
+		}else if (other.tag == "BtmWall"){
+			atBtmWall = true;
+		}
+
+	}
+
+	void onTriggerExit2D(Collider2D other){
+		if(other.tag == "LeftWall"){
+			atLeftWall = false;
+		} else if (other.tag == "RightWall"){
+			atRightWall = false;
+		} else if (other.tag == "TopWall"){
+			atTopWall = false;
+		}else if (other.tag == "BtmWall"){
+			atBtmWall = false;
+		}
+
+
+	}
+
+	void Update () {
+
+		//Vector3 newPos = this.transform.position;
+		//newPos.z += Input.GetAxis ("Mouse ScrollWheel") * sensitivity;
+		//this.transform.position = newPos;
+
+
+	}
+
+	private void FixedUpdate()
+	{
+
+		// Basic camera movement //
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		if (atLeftWall && (moveHorizontal < 0)) {
+			moveHorizontal = 0;
+		}
+		if (atRightWall && (moveHorizontal > 0)) {
+			moveHorizontal = 0;
+		}
+		if (atTopWall && (moveVertical < 0)) {
+			moveVertical = 0;
+		}
+		if (atLeftWall && (moveVertical > 0)) {
+			moveVertical = 0;
+		}
+
+		if (!Input.GetMouseButton (1)) {
+			transform.Translate (new Vector3 (speed * moveHorizontal, 0, 0));
+		}
+
+		transform.Translate (new Vector3 (0, speed * moveVertical, 0));
+
+
+	}
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Planet : MonoBehaviour {
+
+	public int shipRate;
+	public int totalShip = 0;
+	Text text;
+ 
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponentInChildren<Text> ();
+		if (text == null)
+			Debug.Log ("not text field found");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void newTurn (){
+		totalShip += shipRate;
+		text.text = totalShip.ToString();
+
+
+	}
+
+}
