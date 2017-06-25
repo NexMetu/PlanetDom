c04500632e3b0bfe72899cec499dd174ea4f54ca
Matthew Lang <Mattlang9000@gmail.com>
2017-06-26 11:08:25 +1200

    Editting the Canvas

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -8,10 +8,14 @@ public class Planet : MonoBehaviour {
 	public int shipRate;
 	public int totalShip = 0;
 	Text text;
+	Canvas menu;
+	bool selected;
  
 
 	// Use this for initialization
 	void Start () {
+		menu= GetComponentInChildren<Canvas> ();
+		menu.enabled = false;
 		text = GetComponentInChildren<Text> ();
 		if (text == null)
 			Debug.Log ("not text field found");
@@ -20,6 +24,26 @@ public class Planet : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnMouseEnter() {
+		if (!selected) {
+			Debug.Log ("Mouse has entered");
+			menu.enabled = true;
+		}
+	}
+
+	void OnMouseOver() {
+		if (Input.GetMouseButtonDown (0))
+			selected = !selected;
+		
+	}
+
+	void OnMouseExit(){
+		if (!selected) {
+			Debug.Log ("Mouse Exit");
+			menu.enabled = false;
+		}
 	}
 
 	public void newTurn (){
